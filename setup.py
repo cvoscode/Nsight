272c59452f517a1b69326f104f8ca2944f91067f
cvoscode <christoph.a.vos@gmail.com>
2023-03-20 00:10:49 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='nsight',
+   version='1.0',
+   description='A tool to get data Nsights',
+   url='https://github.com/cvoscode/Nsight',
+   author='cvoscode',
+   packages=['nsight'],  #same as name
+   install_requires=['dash', 'dash-bootstrap-components', 'pandas','ridgeplot','scikit-learn','waitress','statsmodels'], #external packages as dependencies
+)
